337dff2061bf60b13a8399a149282aa8d5199e57
Moo4Cash <67906997+Moo4Cash@users.noreply.github.com>
2022-03-29 15:17:21 +0200

    Doneish

--- root/ignore/firstboot.py
+++
@@ -1,6 +0,0 @@
-import pricing
-import json
-
-def firstboot():
-    """Will be run the first time the app is started"""
-    pass
--- root/some_tests.py
+++ root/some_tests.py
@@ -66,3 +66,33 @@ print("debt2", pricing.get_debt(2))
 # scuba.remove_tank(2)
 # scuba.remove_tank(3)
 # scuba.remove_tank(4)
+
+
+
+#------------------------------
+
+# NOT ALLOWED
+pricing.create_pricelist(1)
+pricing.create_pricelist(True)
+pricing.create_pricelist()
+pricing.create_pricelist("")
+pricing.create_pricelist(None)
+
+pricing.update_pricelist(1, "currency", "SEK")
+pricing.update_pricelist(True, "currency", "SEK")
+pricing.update_pricelist("", "currency", "SEK")
+pricing.update_pricelist(None, "currency", "SEK")
+
+pricing.update_pricelist("UDT", "currency", None)
+pricing.update_pricelist("UDT", "currency", 1)
+pricing.update_pricelist("UDT", "currency", True)
+pricing.update_pricelist("UDT", "currency", "")
+pricing.update_pricelist("UDT", "service", -1)
+pricing.update_pricelist("UDT", "service", "dsfa")
+pricing.update_pricelist("UDT", "service", None)
+
+pricing.update_pricelist("UDT", "dasd", "SEK")
+pricing.update_pricelist("UDT", "", "SEK")
+pricing.update_pricelist("UDT", 1, "SEK")
+pricing.update_pricelist("UDT", None, "SEK")
+pricing.update_pricelist("UDT", True, "SEK")
