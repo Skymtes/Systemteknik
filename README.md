aea604802b348ec853ae85b7b9ad90bc781b2ec6
Skymtes <sild20@student.bth.se>
2022-04-08 16:07:41 +0200

    Revert "Wipe"
    This reverts commit fdf35b56ab9c3e328b6656e1c823e15ad283f85b.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Gas Blender
+
+## Introduction
+
+TODO: Add brief introduction.
+
+## Architecture Overview
+
+TODO: Add simple diagram that explains the architecture.
+
+## How to Use
+1. Download the project of the "UI" branch linked here: https://github.com/Skymtes/Systemteknik/tree/UI
+2. With the help of PIP install kivy dependencies with the commands in step 3, 4 and 5. (in order for the imported functions to work (If you do not already have pip visit this site: https://pip.pypa.io/en/stable/cli/pip_install/)
+3. * python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+4. * python -m pip install kivy.deps.gstreamer 
+5. * python -m pip install kivy
+6. Thereafter open up the project, and run the main.py file. This should open an application on your device.
+7. Inserting values in the Old Mix and New Mix tables will now after pressing the button display the values you need to fill. 
+
+
+### Build
+
+TODO: Explain how the whole project can be built.
+
+### Test
+
+TODO: Explain how unit- or integration tests can be executed.
+
+`python3 -m unittest discover` runs all tests.
+
+### Run
+
+TODO: Explain how to run the project (client, server etc.).
+
+## License
+
+TODO: Add license and copyright notice.
---
+++ test/test_blending.py
@@ -0,0 +1,58 @@
+from random import random, randrange
+import sys, os, unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from backend.algorithm.blending import Blend
+
+class BlendingTest(unittest.TestCase):
+
+    def test_blending_eanx32(self):
+            mix = Blend(0.32, 0.0, 175, 0.21, 0.0, 0)
+            expected_oxygen = 24.4
+            expected_helium = 0
+            expected_air = 150.6
+            self.assertEqual(mix, (expected_oxygen, expected_helium, expected_air))  
+
+    def test_blending_eanx36(self):
+            mix = Blend(0.36, 0.0, 175, 0.16, 0.0, 25)
+            expected_oxygen = 34.8
+            expected_helium = 0
+            expected_air = 115.2
+            self.assertEqual(mix, (expected_oxygen, expected_helium, expected_air))
+
+    def test_blending_trimix(self):
+            mix = Blend(0.16, 0.40, 175, 0.10, 0.20, 45)
+            expected_oxygen = 11.4
+            expected_helium = 61.0
+            expected_air = 57.6
+            self.assertEqual(mix, (expected_oxygen, expected_helium, expected_air))    
+
+    def test_blending_zero_values(self):
+            mix = Blend(0.0, 0.0, 0, 0, 0, 0)
+            self.assertEqual(mix, "Can not by divided by zero")
+
+    def test_missing_arguments(self):
+            mix = Blend(0.0, 175, 0.16, 0.40, 45)
+            self.assertEqual(mix,"Invalid input : missing arg")
+
+    def test_string_input(self):
+            mix = Blend(0.36, 0.0, "175", 0.16, 0.40, 45)
+            self.assertEqual(mix,"Invalid input : input as str")
+
+    def test_lower_pressure(self):
+        mix = Blend(0.23, 0.08, 200, 0.35, 0.60, 50)
+        self.assertEqual(mix,(-8.2, -14.0, -4.5))
+
+    def test_range_input(self):
+        test_num = randrange(300,500)
+        idx = randrange(0,5)
+        values = [900,9,158,90,40,287]
+        values[idx] = test_num
+        mix = Blend(*values)
+        self.assertEqual(mix,"Invalid input : range limit")
+    
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    suite = unittest.TestLoader().loadTestsFromTestCase(BlendingTest)
+    runner.run(suite)
