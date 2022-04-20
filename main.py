da262e7044df3c63e39edc2bc556eaff884ae9be
Skymtes <sild20@student.bth.se>
2022-04-20 10:35:41 +0200

    Fixes

--- main.py
+++ main.py
@@ -54,10 +54,13 @@ class HomeScreen(Screen):
             
         if self.ids.new_pressure.text == '':
             self.ids.new_pressure.text = '0'
+
+        if self.ids.tank_capacity.text == '':
+            self.ids.tank_capacity.text = '0'
         
         newBlend = blending.Blend( (float(self.new_otwo.text)) / 100, (float(self.new_he.text)) / 100, float(self.new_pressure.text), (float(self.old_otwo.text)) / 100, (float(self.old_he.text)) / 100, float(self.old_pressure.text)) # Values should be enterd Percentage / Pressure . 
         self.manager.get_screen('more_info_screen').BlendResult(newBlend, self.new_otwo.text, self.new_he.text, self.new_pressure.text, self.old_otwo.text, self.old_he.text, self.old_pressure.text)
-        self.manager.get_screen('more_info_screen').GetPrice(int(self.new_pressure.text), newBlend)
+        self.manager.get_screen('more_info_screen').GetPrice(int(self.ids.tank_capacity.text), newBlend)
 
 class SettingsScreen(Screen):
 
